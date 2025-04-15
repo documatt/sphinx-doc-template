3db5a3bc91299b7d065ffe7e1f7a9fe79ddc44db
Libor Jelínek <liborjelinek@documatt.com>
2025-04-15 22:24:38 +0200

    refactor: non test function move to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
+import difflib
+import os
 import tempfile
 from pathlib import Path
 
 import pytest
+from deep_dircmp import DeepDirCmp
 
 
 @pytest.fixture
@@ -13,3 +16,63 @@ def workaround_tmp_path():
     with tempfile.TemporaryDirectory() as tmpdir:
         tmp_path = Path(tmpdir)
         yield tmp_path
+
+
+def show_diff(file1: Path, file2: Path):
+    """Show the diff between two files."""
+    with open(file1, "r") as f1, open(file2, "r") as f2:
+        f1_lines = f1.readlines()
+        f2_lines = f2.readlines()
+
+    diff = difflib.unified_diff(
+        f1_lines, f2_lines, fromfile=str(file1), tofile=str(file2)
+    )
+    for line in diff:
+        print(line, end="")
+
+
+def deep_compare_dirs(
+    left: Path, right: Path, diff_files=[], left_only=[], right_only=[]
+):
+    """Compare dirs recursively, including file content, and ignore .pickle and .doctree files."""
+    cmp = DeepDirCmp(left, right)
+
+    def ignored_names(name):
+        # binary or files which always differ
+        ignored_names = [".pickle", ".doctree", ".doctrees", ".copier-answers.yml"]
+        return not any(name.endswith(ignored) for ignored in ignored_names)
+
+    def get_diff_files(cmp):
+        """Filter out files based on the ignored names list."""
+        diff_files = list(filter(ignored_names, cmp.get_diff_files_recursive()))
+
+        # show diff of diff_files
+        for file in diff_files:
+            show_diff(cmp.left / file, cmp.right / file)
+
+        return diff_files
+
+    def get_left_files(cmp):
+        return list(filter(ignored_names, cmp.get_left_only_recursive()))
+
+    def get_right_files(cmp):
+        return list(filter(ignored_names, cmp.get_right_only_recursive()))
+
+    assert get_diff_files(cmp) == diff_files, "Different file contents"
+    assert get_left_files(cmp) == left_only, "Left only files"
+    assert get_right_files(cmp) == right_only, "Right only files"
+
+
+def shallow_compare_dirs(dir1, dir2):
+    def get_files(directory):
+        file_set = set()
+        for root, _, files in os.walk(directory):
+            for file in files:
+                relative_path = os.path.relpath(os.path.join(root, file), directory)
+                file_set.add(relative_path)
+        return file_set
+
+    files_in_dir1 = get_files(dir1)
+    files_in_dir2 = get_files(dir2)
+
+    assert files_in_dir1 == files_in_dir2
--- tests/test_bake.py
+++ tests/test_bake.py
@@ -2,81 +2,17 @@
 Tests if generating template works.
 """
 
-import difflib
-import os
 import subprocess
 from pathlib import Path
 
 from copier import run_copy
-from deep_dircmp import DeepDirCmp
+
+from tests.conftest import deep_compare_dirs, shallow_compare_dirs
 
 PROJECT_ROOT = str(Path(__file__).parent.parent)
 
-# Sync with the date in tests/test_bake/defaults/LICENSE
-TEST_YEAR = "2025"
 
-
-def deep_compare_dirs(
-    left: Path, right: Path, diff_files=[], left_only=[], right_only=[]
-):
-    """Compare dirs recursively, including file content, and ignore .pickle and .doctree files."""
-    cmp = DeepDirCmp(left, right)
-
-    def ignored_names(name):
-        # binary or files which always differ
-        ignored_names = [".pickle", ".doctree", ".doctrees", ".copier-answers.yml"]
-        return not any(name.endswith(ignored) for ignored in ignored_names)
-
-    def get_diff_files(cmp):
-        """Filter out files based on the ignored names list."""
-        diff_files = list(filter(ignored_names, cmp.get_diff_files_recursive()))
-
-        # show diff of diff_files
-        for file in diff_files:
-            show_diff(cmp.left / file, cmp.right / file)
-
-        return diff_files
-
-    def get_left_files(cmp):
-        return list(filter(ignored_names, cmp.get_left_only_recursive()))
-
-    def get_right_files(cmp):
-        return list(filter(ignored_names, cmp.get_right_only_recursive()))
-
-    assert get_diff_files(cmp) == diff_files, "Different file contents"
-    assert get_left_files(cmp) == left_only, "Left only files"
-    assert get_right_files(cmp) == right_only, "Right only files"
-
-
-def shallow_compare_dirs(dir1, dir2):
-    def get_files(directory):
-        file_set = set()
-        for root, _, files in os.walk(directory):
-            for file in files:
-                relative_path = os.path.relpath(os.path.join(root, file), directory)
-                file_set.add(relative_path)
-        return file_set
-
-    files_in_dir1 = get_files(dir1)
-    files_in_dir2 = get_files(dir2)
-
-    assert files_in_dir1 == files_in_dir2
-
-
-def show_diff(file1: Path, file2: Path):
-    """Show the diff between two files."""
-    with open(file1, "r") as f1, open(file2, "r") as f2:
-        f1_lines = f1.readlines()
-        f2_lines = f2.readlines()
-
-    diff = difflib.unified_diff(
-        f1_lines, f2_lines, fromfile=str(file1), tofile=str(file2)
-    )
-    for line in diff:
-        print(line, end="")
-
-
-def copier_copy(workaround_tmp_path, **kwargs):
+def call_copier(workaround_tmp_path, **kwargs):
     """Run copier to generate the project."""
     run_copy(
         PROJECT_ROOT,
@@ -94,7 +30,7 @@ def test_default__source(workaround_tmp_path: Path, datadir: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(workaround_tmp_path, defaults=True)
+    call_copier(workaround_tmp_path, defaults=True)
 
     # *** Assert ***
     deep_compare_dirs(workaround_tmp_path, datadir / "default__source")
@@ -105,7 +41,7 @@ def test_default__flat(workaround_tmp_path: Path, datadir: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(workaround_tmp_path, defaults=True, data={"flat": True})
+    call_copier(workaround_tmp_path, defaults=True, data={"flat": True})
 
     # *** Assert ***
     deep_compare_dirs(workaround_tmp_path, datadir / "default__flat")
@@ -116,7 +52,7 @@ def test_with_sample__source(workaround_tmp_path: Path, datadir: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(workaround_tmp_path, defaults=True, data={"with_sample": True})
+    call_copier(workaround_tmp_path, defaults=True, data={"with_sample": True})
 
     # *** Assert ***
     deep_compare_dirs(workaround_tmp_path, datadir / "with_sample__source")
@@ -127,7 +63,7 @@ def test_with_samples__flat(workaround_tmp_path: Path, datadir: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(
+    call_copier(
         workaround_tmp_path, defaults=True, data={"flat": True, "with_sample": True}
     )
 
@@ -139,7 +75,7 @@ def test_no_license(workaround_tmp_path: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(
+    call_copier(
         workaround_tmp_path,
         defaults=True,
         data={"license": "None"},
@@ -156,7 +92,7 @@ def test_nox_build_source(workaround_tmp_path: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(workaround_tmp_path, defaults=True)
+    call_copier(workaround_tmp_path, defaults=True)
 
     # *** Assert ***
     subprocess.run(["nox", "-s", "build"], check=True, cwd=workaround_tmp_path)
@@ -167,7 +103,7 @@ def test_nox_build_flat(workaround_tmp_path: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(workaround_tmp_path, defaults=True, data={"flat": True})
+    call_copier(workaround_tmp_path, defaults=True, data={"flat": True})
 
     # *** Assert ***
     subprocess.run(["nox", "-s", "build"], check=True, cwd=workaround_tmp_path)
@@ -178,7 +114,7 @@ def test_nox_build_all_redirect(workaround_tmp_path: Path, datadir: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(
+    call_copier(
         workaround_tmp_path,
         defaults=True,
         data={"other_languages": ["cs", "he"], "other_builders": ["html"]},
@@ -198,7 +134,7 @@ def test_nox_gettext(workaround_tmp_path: Path, datadir: Path):
     # *** Arrange ***
 
     # *** Act ***
-    copier_copy(
+    call_copier(
         workaround_tmp_path,
         defaults=True,
         data={"other_languages": ["cs", "he"]},
