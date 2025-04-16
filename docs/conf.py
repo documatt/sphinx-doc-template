772c323620fe5e915262289839d190e0fd25e371
Libor Jelínek <liborjelinek@documatt.com>
2025-04-16 22:50:56 +0200

    docs: fix URLs in sitemap.xml

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ mermaid_version = "11.0.1"
 sitemap_locales = [None]
 
 # Default is {lang}{version}{link}, but version is not used in URLs in this project
-sitemap_url_scheme = "{lang}{link}"
+sitemap_url_scheme = "{link}"
 
 # Exclude these files from the sitemap
 # search and genindex are special pages not generated from content
